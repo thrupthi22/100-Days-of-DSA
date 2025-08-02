c539f71426ade02a5a022ba666e1cba9506745e9
Thrupthi S <153356024+thrupthi22@users.noreply.github.com>
2025-08-02 13:21:34 +0530

    reversing a LL

---
+++ DSA.java
@@ -0,0 +1,73 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LL {
+    Node head;
+
+    
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+    }
+
+    // Print the list
+    public void printList() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    // Reverse the list  main logic of this pgm
+    public void reverseList() {
+        if (head == null || head.next == null) {
+            return;
+        }
+        Node prevNode = head;
+        Node currNode = head.next;
+
+        while (currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+
+        System.out.println("Original list:");
+        list.printList();
+
+        list.reverseList();
+
+        System.out.println("Reversed list:");
+        list.printList();
+    }
+}
